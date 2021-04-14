6d5a6cb918b8779b6879302386cf66caace83a44
Joseph Davis <joseph.n.davis.98@gmail.com>
2021-04-14 14:48:04 -0500

    Add more to book detail

--- app/src/main/java/com/example/booktracker/BookDetailFragment.kt
+++ app/src/main/java/com/example/booktracker/BookDetailFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.booktracker.viewmodels.BookDetailViewModel
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,10 +30,23 @@ class BookDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val coverImageView: ImageView = view.findViewById(R.id.detailCoverImageView)
         val titleTextView: TextView = view.findViewById(R.id.detailTitleTextView)
+        val authorTextView: TextView = view.findViewById(R.id.detailAuthorTextView)
+        val publishedDateTextView: TextView = view.findViewById(R.id.detailPublishedDateTextView)
+        val editButton: Button = view.findViewById(R.id.detailEditButton)
 
         viewModel.book.observe(viewLifecycleOwner) { book ->
+            Picasso.get().load(book.imageUrl).error(R.mipmap.ic_default_book).into(coverImageView)
             titleTextView.text = book.title
+            authorTextView.text = book.author
+            publishedDateTextView.text = book.publishedDate.toString()
+        }
+
+        editButton.setOnClickListener {
+            val bookId = arguments?.getInt("bookId")
+            val bundle = bundleOf("bookId" to bookId)
+            findNavController().navigate(R.id.action_bookDetailFragment_to_editBookFragment, bundle)
         }
     }
 }
