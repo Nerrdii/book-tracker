7eb602a85647dfe0cea88dd1055223bf4f77cb17
Joseph Davis <joseph.n.davis.98@gmail.com>
2021-04-17 17:30:28 -0500

    Add more detail to search result detail

--- app/src/main/java/com/example/booktracker/SearchResultDetailFragment.kt
+++ app/src/main/java/com/example/booktracker/SearchResultDetailFragment.kt
@@ -1,15 +1,17 @@
 package com.example.booktracker
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.booktracker.R
 import com.example.booktracker.viewmodels.SearchResultDetailViewModel
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -27,10 +29,28 @@ class SearchResultDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val titleTextView: TextView = view.findViewById(R.id.searchTitleTextView)
+        val coverImageView: ImageView = view.findViewById(R.id.searchResultCoverImageView)
+        val titleTextView: TextView = view.findViewById(R.id.searchResultTitleTextView)
+        val authorTextView: TextView = view.findViewById(R.id.searchResultAuthorTextView)
+        val publishedDateTextView: TextView = view.findViewById(R.id.searchResultPublishedDateTextView)
+        val descriptionTextView: TextView = view.findViewById(R.id.searchResultDescriptionTextView)
+        val genreTextView: TextView = view.findViewById(R.id.searchResultGenreTextView)
+        val addButton: Button = view.findViewById(R.id.searchResultAddButton)
 
-        viewModel.book.observe(viewLifecycleOwner) {
-            Log.d("Detail", it.title)
+        viewModel.book.observe(viewLifecycleOwner) { book ->
+            val imageUrl = if (book.imageUrl?.isEmpty() == true) null else book.imageUrl
+            Picasso.get().load(imageUrl).placeholder(R.mipmap.ic_default_book).error(R.mipmap.ic_default_book).into(coverImageView)
+            titleTextView.text = book.title
+            authorTextView.text = book.author
+            publishedDateTextView.text = book.publishedDate.toString()
+            if (book.description != null) {
+                descriptionTextView.visibility = View.VISIBLE
+                descriptionTextView.text = Html.fromHtml(book.description, Html.FROM_HTML_MODE_COMPACT)
+            }
+            if (book.genre != null) {
+                genreTextView.visibility = View.VISIBLE
+                genreTextView.text = book.genre
+            }
         }
     }
 
